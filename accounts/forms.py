602d9a0d839e79bbffcd87aeed2c6a5ec24283d9
BeomJung <kbeom0124@naver.com>
2024-04-16 10:57:01 +0900

    add login feature 1st commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,12 @@
-from django.contrib.auth import get_user_model
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(
-        max_length=254, help_text='Required. Inform a valid email address.')
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     class Meta:
         model = get_user_model()
-        fields = UserCreationForm.Meta.Fields + ()
+        fields = ('first_name', 'last_name', 'username',
+                  'email', 'profile_picture', 'password1', 'password2')
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
 from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 # Create your views here.
 
 
@@ -13,3 +15,16 @@ def signup(request):
         form = CustomUserCreationForm()
     context = {"form": form}
     return render(request, "accounts/signup.html", context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            next_url = request.GET.get('next') or 'index'
+            return redirect(next_url)
+    else:
+        form = AuthenticationForm()
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
