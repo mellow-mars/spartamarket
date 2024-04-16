fee6255cf5a2f0360fa2b8bf93b61874df574b3a
BeomJung <kbeom0124@naver.com>
2024-04-16 12:28:49 +0900

    updated login and logout feature and added html

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import redirect, render
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.http import require_POST,require_http_methods
 # Create your views here.
 
 
+# @require_http_methods(["GET", "POST"])
 def signup(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
@@ -16,7 +19,7 @@ def signup(request):
     context = {"form": form}
     return render(request, "accounts/signup.html", context)
 
-
+# @require_POST()
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
@@ -28,3 +31,10 @@ def login(request):
         form = AuthenticationForm()
     context = {"form": form}
     return render(request, "accounts/login.html", context)
+
+
+# @require_POST()
+def logout(request):
+    if request.method == "POST":
+        auth_logout(request)
+    return redirect('index')
