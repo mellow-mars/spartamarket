a57bb41519205f481859808e245d1e1d47fcd964
BeomJung <kbeom0124@naver.com>
2024-04-16 16:58:51 +0900

    create read function

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'products'
 urlpatterns = [
-    
+    path('create/',views.create_post,name='create')
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, redirect, render
+from .forms import PostForm
+from .models import Posts
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
 def index(request):
     return render(request, 'products/index.html')
+
+
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect("index")
+    else:
+        form = PostForm()
+    context = {"form": form}
+    return render(request, "products/create_post.html", context)
+
+def post_detail(request):
+    post = get_object_or_404(Posts, pk=pk)
+    context = {'ppst': post}
+    return render(request, 'posts/post_detail.html', context)
