30c67bd921eb7527de8acb9c8eadf37fe01d507e
BeomJung <kbeom0124@naver.com>
2024-04-18 11:34:17 +0900

    added profile pictures

--- spartamarket/urls.py
+++ spartamarket/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from products import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,4 +27,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
     path('users/', include('users.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.contrib.auth import get_user_model
 
+
 # Create your views here.
 def profile(request, username):
     profile = get_object_or_404(get_user_model(), username=username)
